af5f44d272061f3ca5e7b84a69b953e10f139b97
chasewarner99 <chasewarner99@gmail.com>
2017-10-11 17:47:24 -0500

    PRE ALPHA *IN DEVELOPMENT*

---
+++ ChaseW/FoodMenu.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package food.menu;
+
+/**
+ *
+ * @author WARNECHA001
+ */
+import java.io.*;
+import java.util.Scanner;
+public class FoodMenu {
+        public static void main(String[] args) {
+        long food_limit = 0; 
+        System.out.println("what kind of food do you like");
+        long a;
+        Scanner var1 = new Scanner(System.in);
+        while(food_limit == 0){
+        // use \\ for an escape sequence which is uesd to write a file
+        
+        
+        /*
+        make a list add the input to a list call the list for assignment  
+        
+        */
+        
+        }
+    File foodmenu = new File("C:\\Users\\warnecha001\\Food-Menu\\src\\food\\menu");
+    }
+    
+}
