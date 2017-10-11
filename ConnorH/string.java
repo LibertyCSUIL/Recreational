2d6ad8ccb7d4ad5046cc1691372a71fb638d374a
ConnorHardman <connorhardman69@gmail.com>
2017-10-11 16:19:33 -0500

    Version 1.0

---
+++ ConnorH/string.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Larry
+ */
+class string {
+
+    static String format(String of, double result) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
